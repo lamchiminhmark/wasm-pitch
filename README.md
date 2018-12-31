a8e57ae8ab247a8b39621eb74674aaf86dcb2659
Minh Lam <mlam@oberlin.edu>
2018-12-31 16:27:03 +0700

    feat(README): Updated README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # wasm-pitch
-C++ - WASM pitch detection API
+> C++ - WASM pitch detection API
+
+## Example
+```javascript
+    import WasmPitch from 'wasm-pitch';
+
+    const wasmPitch = new WasmPitch();
+    wasmPitch.addListener(/* Callback functions with a pitch parameter goes here */);
+    wasmPitch.start();
+```
+
+## Demo
+Check out the demo directory
---
+++ demo/app.js
@@ -0,0 +1,30 @@
+import WasmPitch from './wasm-pitch';
+
+const buttonEl = document.querySelector('#start');
+const pitchEl = document.querySelector('#pitch');
+const setPitch = pitch => (pitchEl.innerText = pitch >= 0 ? pitch : '__');
+
+// Is it possible for the function to be static?
+// Let's go with non-static => Have to instantiate a wasm-pitch instance to use its functionality
+// wasmPitch has to be a class
+const wasmPitch = new WasmPitch();
+wasmPitch.addListener(setPitch);
+wasmPitch.loaded().then(() => {
+  buttonEl.disabled = false;
+  wasmPitch.addListener(setPitch);
+  // Initialize the button element
+
+  // CONTINUE(ML): setPitch is being called regardless whether started or stopped
+
+  buttonEl.onclick = () => {
+    if (buttonEl.dataset.state === 'off') {
+      buttonEl.innerText = 'Stop';
+      buttonEl.dataset.state = 'on';
+      wasmPitch.start();
+    } else {
+      buttonEl.innerText = 'Start';
+      buttonEl.dataset.state = 'off';
+      wasmPitch.stop();
+    }
+  };
+});
